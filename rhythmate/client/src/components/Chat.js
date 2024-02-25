e3ebde39b867a23e2d0562bb018bdcd61c5049a3
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-02-25 22:33:51 +0530

    New Feature loading...

--- rhythmate/client/src/components/Chat.js
+++ rhythmate/client/src/components/Chat.js
@@ -6,15 +6,15 @@ const Chat = ({descendingOrderMessages}) => {
             <div className="p-1 border bg-indigo-100 h-screen shadow">
                 {descendingOrderMessages.map((message, _index) => (
                     <div key={_index} className="p-3 flex">
-                        <div className="chat-msg-header w-auto  bg-slate-300 flex flex-col ">
+                        <div className="chat-msg-header w-auto items-center flex flex-col ">
                             <div className="pt-2 pl-2 h-10 w-10 truncate rounded-full">
                                 <img src={message.img} alt={message.first_name + 'profile'} className="h-[100%] w-[100%] object-cover rounded-full shadow shadow-black"/>
                             </div>
-                            <p className="bg--200">{message.name}</p>
+                            {/* <p className="bg--200">{message.name}</p> */}
                         </div>
-                        <p className="border pt-2 m-4 rounded-lg ">{message.message}</p>
+                        <p className="border pt-2 p-2 rounded-lg ">{message.message}</p>
                     </div>
-                ))}
+                ))} 
             </div>
         </div>
     )
